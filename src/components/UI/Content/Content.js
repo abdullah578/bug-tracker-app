47bd0c673ca8a32c72c9e789f2c952ff47310ed2
abdullah578 <abdullah.am2000@gmail.com>
2020-06-22 00:18:53 -0400

    used csstransition groups for animations

--- src/components/UI/Content/Content.js
+++ src/components/UI/Content/Content.js
@@ -1,12 +1,23 @@
 import React from "react";
 import classes from "./Content.module.css";
+import CSSTransition from "react-transition-group/CSSTransition";
 
 const Content = (props) => {
-  const styles = [classes.Content];
-  props.open
-    ? styles.push(classes.ContentOpen)
-    : styles.push(classes.ContentClose);
-  return <div className={styles.join(" ")}>{props.children}</div>;
+  return (
+    <CSSTransition
+      in={props.open}
+      classNames={{
+        enter:classes.ContentClose,
+        enterActive: classes.ContentOpen,
+        exitActive: classes.ContentClose,
+      }}
+      timeout={0.3}
+      mountOnEnter
+      unmountOnExit
+    >
+      <div className={classes.Content}>{props.children}</div>
+    </CSSTransition>
+  );
 };
 
 export default Content;
